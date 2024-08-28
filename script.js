115a32db120cc776a11917d692138fba7aefb3a9
Nicolas Junkes <nicolas.junkes00@gmail.com>
2024-08-28 01:27:55 -0300

    feat: create a userCard using axios to fetch data

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const message = document.getElementById("msg");
-const formData = document.getElementById("form");
-
-const insertAfter = (referenceElement, newElement) => {
-  referenceElement.parentNode.insertBefore(
-    newElement,
-    referenceElement.nextSibling
-  );
-};
-
-const errorMessage = (field) => {
-  let errorMessage = document.createElement("span");
-  errorMessage.className = "error-message";
-  errorMessage.textContent = "Please fill out this field.";
-  insertAfter(field, errorMessage);
-};
-
-formData.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  if (username.value === "") {
-    errorMessage(username);
-  }
-  if (email.value === "") {
-    errorMessage(email);
-  }
-  if (message.value === "") {
-    errorMessage(message);
-  } else {
-    alert("Thank you for your message!");
-    console.log({
-      username: username.value,
-      email: email.value,
-      message: message.value,
+const getUser = () => {
+  axios
+    .get("https://api.github.com/users/junkes00")
+    .then((response) => {
+      const users = response.data;
+      console.log(response.data);
+      populateData(users);
+    })
+    .catch((error) => {
+      console.error("An error ocurred while searching for a user", error);
     });
+};
 
-    username.value = "";
-    email.value = "";
-    message.value = "";
-  }
-});
+const populateData = (data) => {
+  const name = document.getElementById("name");
+  const username = document.getElementById("username");
+  const currentCompany = document.getElementById("company");
+  const bio = document.getElementById("bio");
+
+  name.textContent = data.name;
+  username.textContent = data.login;
+  currentCompany.textContent = data.company;
+  bio.textContent = `Bio: ${data.bio}`;
+};
+
+const getRepos = () => {
+  axios
+    .get("https://api.github.com/users/junkes00/repos")
+    .then((response) => {
+      const repos = response.data.length;
+      console.log(response.data);
+      populateReposData(repos);
+    })
+    .catch((error) =>
+      console.error("An error ocurred while searching for the repos", error)
+    );
+};
+
+const populateReposData = (data) => {
+  const publicRepos = document.getElementById("public-repos");
+  publicRepos.textContent = `Public repos: ${data}`;
+};
+
+getUser();
+getRepos();
